b50ca9e406d0739fb0f18eac4bd06198f5f328ed
Austin M. Matherne <Austin.Matherne@Workiva.com>
2025-08-05 17:38:54 -0400

    Run taxonomy packages suite with match-all

--- tests/integration_tests/validation/conformance_suite_configurations/xbrl_taxonomy_packages_1_0.py
+++ tests/integration_tests/validation/conformance_suite_configurations/xbrl_taxonomy_packages_1_0.py
@@ -8,8 +8,18 @@ config = ConformanceSuiteConfig(
             entry_point=Path('index.xml'),
         ),
     ],
+    expected_testcase_errors={f'index.xml:{s}': val for s, val in {
+        'V-003-missing-meta-inf-directory': {
+            'tpe:metadataFileNotFound': 1,
+        },
+        'V-008-invalid-missing-xml-lang': {
+            'tpe:missingLanguageAttribute': 1,
+        },
+        'V-011-invalid-path-separator': {
+            'tpe:invalidDirectoryStructure': -1,
+        }
+    }.items()},
     info_url='https://specifications.xbrl.org/work-product-index-taxonomy-packages-taxonomy-packages-1.0.html',
     membership_url='https://www.xbrl.org/join',
     name=PurePath(__file__).stem,
-    test_case_result_options='match-any',
 )
