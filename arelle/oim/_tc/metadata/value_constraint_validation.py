e8f984c4beeb9f1f8b102802c6a28047b7456f84
Austin M. Matherne <Austin.Matherne@Workiva.com>
2026-04-30 01:07:08 -0400

    Validate applicable facets

--- arelle/oim/_tc/metadata/value_constraint_validation.py
+++ arelle/oim/_tc/metadata/value_constraint_validation.py
@@ -6,13 +6,26 @@ from __future__ import annotations
 
 from collections.abc import Generator, Mapping
 
-from arelle.oim._tc.const import TCME_UNKNOWN_TYPE
+from arelle.ModelValue import QName
+from arelle.oim._tc.const import TCME_ILLEGAL_CONSTRAINT, TCME_UNKNOWN_TYPE
 from arelle.oim._tc.metadata.common import TCMetadataValidationError
 from arelle.oim._tc.metadata.model import TCValueConstraint
+from arelle.oim._tc.metadata.restrictions import TCRestriction, applicable_restrictions
 from arelle.oim._tc.metadata.types import resolve_effective_lexical_type
 from arelle.typing import TypeGetText
 
 _: TypeGetText
+
+
+class TCMetadataIllegalConstraintError(TCMetadataValidationError):
+    def __init__(self, message: str, *paths: str) -> None:
+        primary, *related = paths
+        super().__init__(
+            message,
+            primary,
+            code=TCME_ILLEGAL_CONSTRAINT,
+            related_paths=[(r,) for r in related],
+        )
 
 
 def validate_value_constraint(
@@ -31,3 +44,23 @@ def validate_value_constraint(
             code=TCME_UNKNOWN_TYPE,
         )
         return
+    yield from _validate_restrictions_applicability(constraint, effective_lexical_type)
+
+
+def _validate_restrictions_applicability(
+    constraint: TCValueConstraint,
+    effective_lexical_type: QName,
+) -> Generator[TCMetadataValidationError, None, None]:
+    applied_restrictions = {
+        restriction for restriction in TCRestriction if getattr(constraint, restriction.attr_name, None) is not None
+    }
+    permitted_restrictions = applicable_restrictions(constraint.type, effective_lexical_type)
+    if disallowed_restrictions := sorted(applied_restrictions - permitted_restrictions):
+        yield TCMetadataIllegalConstraintError(
+            _("Constraint of type '{}' must not define restrictions '{}'").format(
+                constraint.type,
+                ", ".join(disallowed_restrictions),
+            ),
+            "type",
+            *disallowed_restrictions,
+        )
